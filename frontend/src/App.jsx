2e1468c086d6c70e552caf600a7dbac0e5a287de
Jordan Assayah <jassayah@makor-capital.com>
2023-06-12 13:01:58 +0300

    feature-#5 Add Authentication page + create component AuthForm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
+import AuthenticationView from "./views/Authentication"
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <AuthenticationView/>
 }
 
 export default App
