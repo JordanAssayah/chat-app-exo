39286973f727075394d5c39961ad44346904b053
Jordan Assayah <jordanassayah@gmail.com>
2023-06-15 08:57:53 +0300

    bug-#17 Fix auth + add functions to get messages and listen for events

--- frontend/src/features/auth/AuthForm.jsx
+++ frontend/src/features/auth/AuthForm.jsx
@@ -1,52 +1,70 @@
-import { useDispatch } from 'react-redux'
-import { useNavigate, Link } from 'react-router-dom'
-import { setUserName } from './authSlice'
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+
+import { SocketIoContext } from '../../utils/contexts'
 
 const AuthForm = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate();
+  const socketIoContext = useContext(SocketIoContext)
 
   const onSubmitForm = (evt) => {
     evt.preventDefault()
-    dispatch(setUserName(evt.target.username.value))
-    navigate('/dashboard')
+    socketIoContext.authenticate({
+      username: evt.target.username.value,
+      password: evt.target.password.value,
+    })
   }
 
   return (
-    <form className="space-y-6" onSubmit={onSubmitForm}>
-      <div>
-        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-          Username
-        </label>
-        <div className="mt-2">
-          <input
-            type="text"
-            name="username"
-            id="username"
-            autoComplete="username"
-            required
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
+    <>
+      <form className="space-y-6" onSubmit={onSubmitForm}>
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+            Username
+          </label>
+          <div className="mt-2">
+            <input
+              id="username"
+              name="username"
+              type="text"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
         </div>
-      </div>
 
-      <div>
-        <button
-          type="submit"
-          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Start to chat
-        </button>
-      </div>
-      <div>
-        <Link
-          to={'/register'}
-          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Create an account
+        <div>
+          <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+            Password
+          </label>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+
+      <p className="mt-10 text-center text-sm text-gray-500">
+        Not a member?{' '}
+        <Link to={'/register'} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+          Create account
         </Link>
-      </div>
-    </form>
+      </p>
+    </>
   )
 }
 
