4cb0b36b169bac9b4fab8819c530939c0608ac8f
Jordan Assayah <jordanassayah@gmail.com>
2023-06-15 08:55:47 +0300

    bug-#17 Update the dashboard page so that it shows messages for the correct channel

--- frontend/src/routes/Authentication.jsx
+++ frontend/src/routes/Authentication.jsx
@@ -6,10 +6,10 @@ function Authentication() {
   return (
     <SocketIoConnector>
       <div className="flex min-h-full flex-1 items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
-        <div className="w-full max-w-sm space-y-10">
+        <div className="w-full max-w-sm space-y-5">
           <div>
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Pick a username
+            <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign in to your account
             </h2>
           </div>
           <AuthForm />
